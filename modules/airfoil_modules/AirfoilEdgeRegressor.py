97c720f7d6f80e93c14b66e80327805e1a9be322
LSaldyt <lucassaldyt@gmail.com>
2020-03-24 12:11:20 -0400

    Removes convolutions and changes learning rate, resulting in
    improvements in loss progress

--- modules/airfoil_modules/AirfoilEdgeRegressor.py
+++ modules/airfoil_modules/AirfoilEdgeRegressor.py
@@ -15,27 +15,25 @@ from time import sleep
 from PIL import Image
 
 class EdgeRegressorModel(nn.Module):
-    def __init__(self, depth=1, kernel_size=3, start_channels=4, mid_channels=10, pool_size=2, activation=nn.ReLU):
+    def __init__(self, depth=1, activation=nn.ReLU, mid_size=10000, out_size=600):
         nn.Module.__init__(self)
-        self.mid_channels = mid_channels
         self.conv_layers = []
         for i in range(depth):
             if i == 0:
-                in_channels = start_channels
+                insize = 224 * 224 * 4
             else:
-                in_channels = mid_channels
+                insize = mid_size
 
             self.conv_layers.append(nn.Sequential(
-                nn.Conv2d(in_channels, mid_channels, kernel_size),
-                nn.MaxPool2d(pool_size, pool_size),
+                nn.Linear(insize, mid_size),
                 activation()
                 ))
-        self.final = nn.Sequential(nn.Linear(mid_channels * 26 * 26, 100), nn.Linear(100, 1000))
+        self.final = nn.Sequential(nn.Linear(mid_size, out_size))
 
     def forward(self, x):
+        x = x.view(224 * 224 * 4)
         for layer in self.conv_layers:
             x = layer(x)
-        x = x.view(-1, 26 * 26 * self.mid_channels)
         x = self.final(x)
         x = x.double().squeeze(dim=0)
         return x
@@ -43,7 +41,7 @@ class EdgeRegressorModel(nn.Module):
 class AirfoilEdgeRegressor(OnlineTorchLearner):
     def __init__(self, filename='data/models/airfoil_edge_regressor.nn', name='AirfoilEdgeRegressor'):
         self.driver = None
-        optimizer_kwargs = dict(lr=0.0001, momentum=0.9)
+        optimizer_kwargs = dict(lr=0.01, momentum=0.9)
         OnlineTorchLearner.__init__(self, nn.MSELoss, optim.SGD, optimizer_kwargs, in_label='AirfoilPlot', name=name, filename=filename)
 
     def load_image(self, node):
@@ -57,11 +55,12 @@ class AirfoilEdgeRegressor(OnlineTorchLearner):
         parent = self.driver.get(node.data['parent'])
         with open(parent['coord_file'], 'rb') as infile:
             coordinates = pickle.load(infile)
-        coordinates = sum(map(list, coordinates), [])
+        fx, fy, sx, sy, camber = coordinates
+        coordinates = sum(map(list, [fx, fy, sy]), [])
         return torch.tensor(coordinates, dtype=torch.double)
 
     def init_model(self):
-        self.model = EdgeRegressorModel(depth=3)
+        self.model = EdgeRegressorModel(depth=1)
 
     def transform(self, node):
         labels = self.load_labels(node)
