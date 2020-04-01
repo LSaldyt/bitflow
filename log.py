6d9d22ab6c7d2b676e5588a3c163eaa0351aaaf9
LSaldyt <lucassaldyt@gmail.com>
2020-04-01 14:54:56 -0400

    Adds logging class

---
+++ log.py
@@ -0,0 +1,26 @@
+
+import os, datetime
+
+LOG_DIR = 'logs/'
+
+class Log:
+    def __init__(self, name, directory=None, clear=True):
+        if not os.path.isdir('logs'):
+            os.mkdir('logs')
+        if directory is not None:
+            if not os.path.isdir(directory):
+                os.mkdir(LOG_DIR + directory)
+            self.path = LOG_DIR + directory + '/' + name
+        else:
+            self.path = LOG_DIR + name
+        self.time = datetime.now()
+        self.path += '_' + self.time.strftime('%a_%d_%b_%y_%I_%M_%p') + '.log'
+        if clear and os.path.isfile(self.path):
+            os.remove(self.path)
+        elif not os.path.isfile(self.path):
+            with open(self.path, 'w') as outfile:
+                outfile.write('')
+
+    def log(self, message, end='\n'):
+        with open(self.path, 'a') as outfile:
+            outfile.write(message + end)
