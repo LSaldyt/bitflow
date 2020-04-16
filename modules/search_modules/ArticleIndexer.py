2c837f81f54ac3e8f71f9e35c4b8338e90599377
LSaldyt <lucassaldyt@gmail.com>
2020-04-16 14:21:53 -0400

    Adds basic article indexer

---
+++ modules/search_modules/ArticleIndexer.py
@@ -0,0 +1,41 @@
+from petal.pipeline.module_utils.module import Module
+from ..libraries.natural_language.cleaner import Cleaner
+
+class HitList:
+    def __init__(self):
+        self.sections = dict()
+        self.words    = set()
+
+    def add(self, section, word):
+        self.words.add(word)
+
+        if section not in self.sections:
+            self.sections[section] = dict()
+        section_counter = self.sections[section]
+        if word in section_counter:
+            section_counter[word] += 1
+        else:
+            section_counter[word] = 1
+
+
+class ArticleIndexer(Module):
+    '''
+    This module is intended to index articles within PeTaL
+    '''
+    def __init__(self, in_label='Article', out_label='HitList', connect_labels=('hitlist', None), name='Wikipedia'):
+        Module.__init__(self, in_label, out_label, connect_labels, name, page_batches=True)
+        self.SECTIONS = {'title', 'summary'}
+        self.cleaner = Cleaner()
+
+    def process(self, previous):
+        self.log.log('Running Indexer')
+
+        hitlist = HitList()
+
+        data = previous.data
+        for section in self.SECTIONS:
+            text = data[section]
+            for word in cleaner.clean():
+                hitlist.add(section, word)
+
+        return self.default_transaction(data=hitlist.sections)
